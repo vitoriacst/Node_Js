7c8929e721fd144ffc761cacacfad27869e2cc27
vitoriacst <vitoria.cst.holanda@gmail.com>
2022-08-01 15:21:10 -0300

    feat: :construction: learning to export and import function in module

---
+++ First_steps/brlValues.js
@@ -0,0 +1,8 @@
+// brlValue.js
+const brl = 5.37;
+
+const usdToBrl = (valueInUsd) => valueInUsd * brl;
+
+module.exports = usdToBrl;
+
+// aprendendo a exportar uma func
---
+++ First_steps/index.js
@@ -0,0 +1,5 @@
+const convert = require('./brlValues');
+// aqui nos estamos importando a func que foi exportada no modulo anterior
+
+const usd = 10;
+const brl = convert(usd);
